f5fe98f2a5ef2807047e6c611ab28c574c8af797
chrisvasqm <christianvasquezmelo@gmail.com>
2023-09-24 19:14:08 -0400

    Add options bar

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, Image, Text } from '@chakra-ui/react';
+import { Box, Button, Heading, Image, Text } from '@chakra-ui/react';
 import Game from '../models/Game';
 
 interface Props {
@@ -15,7 +15,31 @@ function GameCard({ game }: Props) {
       overflow='hidden'
       boxShadow='xl'
     >
-      <Image src={game.image} width={'100%'} objectFit={'cover'} height={'200px'} />
+      <Box position='relative'>
+        <Image src={game.image} width={'100%'} objectFit={'cover'} height={'200px'} />
+        <Box
+          padding={0}
+          position='absolute'
+          bottom='0'
+          left='0'
+          width='100%'
+          backgroundImage='linear-gradient(to top, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0))'
+          display='flex'
+          gap={2}
+          justifyContent='start'
+          alignItems='center'
+        >
+          <Button variant='solid' colorScheme='teal'>
+            Playing
+          </Button>
+          <Button variant='solid' colorScheme='teal'>
+            Backlog
+          </Button>
+          <Button variant='solid' colorScheme='teal'>
+            Beaten
+          </Button>
+        </Box>
+      </Box>
       <Box padding={6}>
         <Heading>{game.name}</Heading>
         <Text>{game.description}</Text>
