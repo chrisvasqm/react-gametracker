91e986841afcc3514e069a1e061cee5b28decfcf
chrisvasqm <christianvasquezmelo@gmail.com>
2023-09-24 18:54:54 -0400

    Display image

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, Text } from '@chakra-ui/react';
+import { Box, Heading, Image, Text } from '@chakra-ui/react';
 import Game from '../models/Game';
 
 interface Props {
@@ -7,16 +7,12 @@ interface Props {
 
 function GameCard({ game }: Props) {
   return (
-    <Box
-      padding={6}
-      width={'100%'}
-      borderWidth='1px'
-      borderRadius='lg'
-      overflow='hidden'
-      boxShadow='xl'
-    >
-      <Heading>{game.name}</Heading>
-      <Text>{game.description}</Text>
+    <Box width={'100%'} borderWidth='1px' borderRadius='lg' overflow='hidden' boxShadow='xl'>
+      <Image src={game.image} width={'100%'} objectFit={'cover'} height={'200px'} />
+      <Box padding={6}>
+        <Heading>{game.name}</Heading>
+        <Text>{game.description}</Text>
+      </Box>
     </Box>
   );
 }
